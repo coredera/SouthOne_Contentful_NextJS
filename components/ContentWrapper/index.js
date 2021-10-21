1960023c0aabf81a601eddb261d8bef5a91e256c
Jason Fang <jason.fang@guidedogs.org.uk>
2021-10-21 18:38:38 +0100

    chakra install

--- components/ContentWrapper/index.js
+++ components/ContentWrapper/index.js
@@ -1,8 +1,5 @@
-//import ContentWrapperStyles from "@styles/ContentWrapper.module.css";
-import { ChakraProvider } from "@chakra-ui/react";
-import Fonts from "/pages/_fonts";
+import ContentWrapperStyles from "@styles/ContentWrapper.module.css";
 
 export default function ContentWrapper({ children }) {
-  return <div className={ChakraProvider}><Fonts />{children}</div>;
-  //return <div className={ContentWrapperStyles.container}>{children}</div>;
+  return <div className={ContentWrapperStyles.container}>{children}</div>;
 }
