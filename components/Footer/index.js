00b0c61ba083b4a260c67809b9fddc6a9a531ec3
Jason Fang <jason.fang@guidedogs.org.uk>
2021-11-04 14:18:02 +0000

    chakra WIP

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -14,6 +14,8 @@ import {
   Spacer,
   Icon,
   Button,
+  VStack,
+  Text,
 } from "@chakra-ui/react";
 
 import Link from "next/link";
@@ -23,44 +25,116 @@ export default function Footer() {
 
   return (
     <footer className={FooterStyles.footer}>
-      <Flex>
-        <Box>
-          <Heading fontSize="2.125rem">Useful links</Heading>
-          Want to find out more about Guide Dogs and sight loss? Here are other
-          helpful links...
-          
-          <SimpleGrid columns="2">
-            <GridItem colspac="1">
-              <Flex className={FooterStyles.links}>
-              <img src="./images/icon-arrow-blue-tint.svg" className={FooterStyles.icons} />  
-              <Link href="https://www.guidedogs.org.uk/faqs/">
-              FAQs  
-              </Link>        
-              </Flex>
-            </GridItem>
-            <GridItem colspac="1">two</GridItem>
-          </SimpleGrid>
+      <Flex pt={8} pb={10} className={FooterStyles.linkpanels}>
+        <Spacer />
+        <Box maxWidth="475">
+          <VStack spacing={3} alignItems="flex-start" pr={12} className={FooterStyles.leftlinkpanel}>
+            <Heading fontSize="2.125rem">Useful links</Heading>
+            <Text>
+              Want to find out more about Guide Dogs and sight loss? Here are
+              other helpful links...
+            </Text>
+            <SimpleGrid columns="2" pt={5} width="100%">
+              <GridItem colspac="1">
+                <Flex className={FooterStyles.links}>
+                  <Box pr={3}>
+                    <img
+                      src="./images/icon-arrow-blue-tint.svg"
+                      className={FooterStyles.icons}
+                    />
+                  </Box>
+                  <Link href="https://www.guidedogs.org.uk/faqs/">FAQs</Link>
+                </Flex>
+              </GridItem>
+              <GridItem colspac="1">
+                <Flex className={FooterStyles.links}>
+                  <Box pr={3}>
+                    <img
+                      src="./images/icon-arrow-blue-tint.svg"
+                      className={FooterStyles.icons}
+                    />
+                  </Box>
+                  <Link href="https://www.guidedogs.org.uk/news/">News</Link>
+                </Flex>
+              </GridItem>
+            </SimpleGrid>
+            <SimpleGrid columns="2" pt={5} width="100%">
+              <GridItem colspac="1">
+                <Flex className={FooterStyles.links}>
+                  <Box pr={3}>
+                    <img
+                      src="./images/icon-arrow-blue-tint.svg"
+                      className={FooterStyles.icons}
+                    />
+                  </Box>
+                  <Link href="https://www.guidedogs.org.uk/about-us/careers/">Jobs</Link>
+                </Flex>
+              </GridItem>
+              <GridItem colspac="1">
+                <Flex className={FooterStyles.links}>
+                  <Box pr={3}>
+                    <img
+                      src="./images/icon-arrow-blue-tint.svg"
+                      className={FooterStyles.icons}
+                    />
+                  </Box>
+                  <Link href="https://www.guidedogs.org.uk/inspiring-stories/">Inpsiring stories</Link>
+                </Flex>
+              </GridItem>
+            </SimpleGrid>
+          </VStack>
         </Box>
-        <Box>
-          <Heading fontSize="2.125rem">Keep in touch</Heading>
-          Do you have an enquiry? Why not contact a member of our friendly
-          staff?
+        <Box maxWidth="475">
+          <VStack spacing={3} alignItems="flex-start" pl={6} pr={6} className={FooterStyles.leftlinkpanel}>
+            <Heading fontSize="2.125rem">Keep in touch</Heading>
+            <Text>
+              Do you have an enquiry? Why not contact a member of our friendly
+              staff?
+            </Text>
+            <Flex className={FooterStyles.links} pt={5}>
+                  <Box pr={3}>
+                    <img
+                      src="./images/icon-arrow-blue-tint.svg"
+                      className={FooterStyles.icons}
+                    />
+                  </Box>
+                  <Link href="https://www.guidedogs.org.uk/contact-us/">Contact us</Link>
+                </Flex>
+                <Flex className={FooterStyles.links}>
+                  <Box pr={3}>
+                    <img
+                      src="./images/icon-email-blue-tint.svg"
+                      className={FooterStyles.icons}
+                    />
+                  </Box>
+                  <Link href="https://www.guidedogs.org.uk/contact-us/email-updates/">Sign up to our email updates</Link>
+                </Flex>
+          </VStack>
         </Box>
-        <Box>
-          <Heading fontSize="2.125rem">Social media</Heading>
-          Want to keep up to date with all the latest at Guide Dogs? Follow us
-          on social media.
+        <Box maxWidth="475">
+          <VStack spacing={3} alignItems="flex-start" pl={12}>
+            <Heading fontSize="2.125rem">Social media</Heading>
+            <Text>
+              Want to keep up to date with all the latest at Guide Dogs? Follow
+              us on social media.
+            </Text>
+          </VStack>
         </Box>
+        <Spacer />
+        
       </Flex>
-      <Flex>
+      <Flex pt={5}>
         <Box width="50%">
+          <Box fontSize="lg" fontWeight="bold" pb={5}>
           © The Guide Dogs for the Blind Association 2021
-          <p></p>
+          </Box>
+          <Box fontWeight="normal">
           Guide Dogs is a working name of The Guide Dogs for the Blind
           Association. Registered Office: Hillfields, Burghfield Common,
           Reading, Berkshire RG7 3YG. A company limited by guarantee registered
           in England and Wales (291646) and a charity registered in England and
           Wales (209617) and Scotland (SC038979)
+          </Box>
         </Box>
         <Box width="25%" alignItems="center">
           <SimpleGrid columns="2">
