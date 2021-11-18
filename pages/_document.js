7a76a5e69425ce3d31d593bdd51d44f39d7d6e94
Jason Fang <jason.fang@guidedogs.org.uk>
2021-11-18 17:30:13 +0000

    added GTM

--- pages/_document.js
+++ pages/_document.js
@@ -12,9 +12,6 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 //import {Fonts} from "./Fonts.js";
 //import { gtheme } from "../styles/customTheme/theme";
 
-
-
-
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -24,15 +21,35 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-         
-        <Head />
+        <Head>
+          {/* Google Tag Manager - Global base code */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer', '${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}');
+               `,
+            }}
+          />
+        </Head>
+
         <body>
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
+
           <Main />
           <NextScript />
         </body>
-      
       </Html>
-      
     );
   }
 }
