f695853ff8a47e8a43f16925b2a3983674312071
Jason Fang <jason.fang@guidedogs.org.uk>
2021-11-19 15:25:54 +0000

    GTM

--- pages/_document.js
+++ pages/_document.js
@@ -27,10 +27,10 @@ class MyDocument extends Document {
             dangerouslySetInnerHTML={{
               __html: `
               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer', '${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}');
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WKM32MM');
                `,
             }}
           />
@@ -39,7 +39,7 @@ class MyDocument extends Document {
         <body>
           <noscript>
             <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}`}
+              src="https://www.googletagmanager.com/ns.html?id=GTM-WKM32MM"
               height="0"
               width="0"
               style={{ display: "none", visibility: "hidden" }}
