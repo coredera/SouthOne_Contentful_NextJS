70cad1251eff6ea8cd8db89df2c592b405cf675f
Jason Fang <jason.fang@guidedogs.org.uk>
2021-11-25 15:59:53 +0000

    fonts.js moved

--- components/Container/index.js
+++ components/Container/index.js
@@ -1,6 +1,6 @@
 //import ContentWrapperStyles from "@styles/ContentWrapper.module.css";
 import { ChakraProvider } from "@chakra-ui/react";
-import Fonts from "/pages/Fonts";
+import Fonts from "../styles/Fonts";
 
 const Container = ({ children }) => {
     
