1c4410e1c4cd8ce23120cb6e66096837d068bca0
Jason Fang <jason.fang@guidedogs.org.uk>
2021-12-07 21:10:28 +0000

    updated robots.txt

--- next-sitemap.js
+++ next-sitemap.js
@@ -2,7 +2,8 @@ module.exports = {
   siteUrl: "https://www.guidedogs.org.uk",
   generateRobotsTxt: true, // (optional)
   robotsTxtOptions: {
-    policies: [{ userAgent: "*", disallow: "/api" }],
+   // policies: [{ userAgent: "*", disallow: "/api" }],
+   policies: [{ userAgent: "*", disallow: "/" }],
   },
   exclude: ["/api/*", "/server-sitemap.xml"],
   robotsTxtOptions: {
