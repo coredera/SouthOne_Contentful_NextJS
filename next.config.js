c9c25baec4499b5c229a4665b4d8fc20e4492ad5
Jason Fang <jason.fang@guidedogs.org.uk>
2022-01-07 09:49:08 +0000

    next.config included domains for mainsite, dev,stg

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   basePath: '/blog',
   images: {
-    domains: ["images.ctfassets.net", "gd-blog.netlify.app"],
+    domains: ["images.ctfassets.net", "gd-blog.netlify.app", "www.guidedogs.org.uk", "gd-blog-dev.netlify.app", "gd-blog-staging.netlify.app"],
   },
 };
