cde84103367109c12b152cea0d4c36e221e6feb4
Jason Fang <jason.fang@guidedogs.org.uk>
2022-01-11 17:53:55 +0000

    assetPrefix, remove ref to OG image

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   basePath: '/blog',
+  assetPrefix: '/blog',
   images: {
     domains: ["images.ctfassets.net", "gd-blog.netlify.app", "www.guidedogs.org.uk", "gd-blog-dev.netlify.app", "gd-blog-staging.netlify.app"],
   },
