ef046a4d2385b286f99e677b91220306747970ff
Jason Fang <jason.fang@guidedogs.org.uk>
2022-01-11 18:44:56 +0000

    Updated Next JS to v12

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   basePath: '/blog',
-  assetPrefix: '/blog',
+  //assetPrefix: '/blog',
   images: {
     domains: ["images.ctfassets.net", "gd-blog.netlify.app", "www.guidedogs.org.uk", "gd-blog-dev.netlify.app", "gd-blog-staging.netlify.app"],
   },
