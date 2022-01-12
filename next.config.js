6f2c07581f75d60b565d075b30f5afef439b588c
Jason Fang <jason.fang@guidedogs.org.uk>
2022-01-12 20:51:36 +0000

    netlify config removed cross origin anon

--- next.config.js
+++ next.config.js
@@ -5,5 +5,5 @@ module.exports = {
   images: {
     domains: ["images.ctfassets.net", "gd-blog.netlify.app", "www.guidedogs.org.uk", "gd-blog-dev.netlify.app", "gd-blog-staging.netlify.app"],
   },
-  crossOrigin: 'anonymous'
+  //crossOrigin: 'anonymous'
 };
