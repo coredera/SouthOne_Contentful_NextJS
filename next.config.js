06f8684e48be3a07c3ed13ddfebc1f779279248a
Jason Fang <jason.fang@guidedogs.org.uk>
2022-01-12 22:51:10 +0000

    added exportpathmap for 4th and volunteer blog

--- next.config.js
+++ next.config.js
@@ -6,4 +6,20 @@ module.exports = {
     domains: ["images.ctfassets.net", "gd-blog.netlify.app", "www.guidedogs.org.uk", "gd-blog-dev.netlify.app", "gd-blog-staging.netlify.app"],
   },
   //crossOrigin: 'anonymous'
+
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      
+      '/blog/4th-blog-post': { page: '/blog', query: { title: '4th-blog-post' } },
+      '/blog/volunteer': { page: '/blog', query: { title: 'volunteer' } },
+     
+    }
+  },
+
+
+
 };
