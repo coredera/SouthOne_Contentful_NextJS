465fa0cce9a5fee10f89ee1ac08b03c9021674e3
Jason Fang <jason.fang@guidedogs.org.uk>
2022-03-23 21:57:07 +0000

    Blog landing page design update

--- components/PostList/index.js
+++ components/PostList/index.js
@@ -34,67 +34,91 @@ export default function PostList(props) {
   const { postListType, posts, currentPage, totalPages } = props;
   const nextDisabled = parseInt(currentPage, 10) === parseInt(totalPages, 10);
   const prevDisabled = parseInt(currentPage, 10) === 1;
- 
-  return ( 
-    <>
-     
 
+  return (
+    <>
       <ol className={ContentListStyles.contentList}>
         {posts.map((post) => (
           <li key={post.sys.id}>
-            <article className={ContentListStyles.contentList__post}>
-              <Link href={`/${post.slug}`}>
-                <a>
-                  <img
-                    src={post.image.url}
-                    width={post.image.width}
-                    height={post.image.height}
-                    layout="responsive" 
-                    objectFit="contain"
-                    alt={post.image.description}
-                    style={{borderRadius:'20px'}}
-                  /> 
-                </a>
-              </Link> 
-
-              <Flex p={2} />
-              <Link href={`/${post.slug}`}>
-                <a className={ContentListStyles.contentList__titleLink}>
-                  <h2 className={ContentListStyles.contentList__title}>
-                    {post.title}
-                  </h2>
-                </a>
-              </Link>
-              <Box className={ContentListStyles.contentList__author}>
-                {post.author !== null && <> Author: {post.author.name}</>}
-              </Box>
-              {post.contentfulMetadata.tags !== null && (
-                <Tags tags={post.contentfulMetadata.tags} />
-              )}
-              <div className={ContentListStyles.contentList__excerpt}>
-                <ReactMarkdown
-                  children={post.excerpt}
-                  renderers={ReactMarkdownRenderers(post.excerpt)}
-                />
-              </div>
-              <Flex alignItems="center">
-                <Box alignSelf="center">
+            <Box bgColor="white" style={{ borderRadius: "20px" }}>
+              <article className={ContentListStyles.contentList__post}>
+                <Box>
                   <Link href={`/${post.slug}`}>
                     <a>
-                      <h3
-                        className={ContentListStyles.contentList__readmorelink}
-                      >
-                        Read more
-                      </h3>
+                      <img
+                        src={post.image.url}
+                        width="1200"
+                        height="200"
+                        layout="responsive"
+                        objectFit="cover"
+                        alt={post.image.description}
+                        style={{
+                          borderTopLeftRadius: "20px",
+                          borderTopRightRadius: "20px",
+                          objectFit: "cover",
+                          height: "500px",
+                        }}
+                      />
                     </a>
                   </Link>
                 </Box>
-                <Spacer />
-                <Box alignSelf="center" className={TypographyStyles.bodyCopy}>
-                  <PublishedDate date={post.date} alignSelf="center" />
+
+                <Box p={10} pb={12}>
+                  <Flex pb={4}>
+                    <Box>
+                      <Link href={`/${post.slug}`}>
+                        <a>
+                          <h2 className={ContentListStyles.contentList__title}>
+                            {post.title}
+                          </h2>
+                        </a>
+                      </Link>
+                    </Box>
+                    <Spacer />
+                    <Box
+                      minWidth="180"
+                      alignSelf="-moz-initial"
+                      textAlign="right"
+                    
+                      className={TypographyStyles.bodyCopy}
+                    >
+                      <PublishedDate date={post.date} alignSelf="center" />
+                    </Box>
+                  </Flex>
+
+                  <Box className={ContentListStyles.contentList__author}>
+                    {post.author !== null && <> <b> Author: {post.author.name} </b></>}
+                  </Box>
+                  <Flex p={1} />
+                  {post.contentfulMetadata.tags !== null && (
+                    <Tags tags={post.contentfulMetadata.tags} />
+                  )}
+                  <Box pb={5} pt={3}>
+                    <div className={ContentListStyles.contentList__excerpt}>
+                      <ReactMarkdown
+                        children={post.excerpt}
+                        renderers={ReactMarkdownRenderers(post.excerpt)}
+                      />
+                    </div>
+                  </Box>
+                  <Flex alignItems="center">
+                    <Box alignSelf="center">
+                      <Link href={`/${post.slug}`}>
+                        <a>
+                          <h3
+                            className={
+                              ContentListStyles.contentList__readmorelink
+                            }
+                          >
+                            Read more
+                          </h3>
+                        </a>
+                      </Link>
+                    </Box>
+                  </Flex>
                 </Box>
-              </Flex>
-            </article>
+              </article>
+            </Box>
           </li>
         ))}
       </ol>
