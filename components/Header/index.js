0606b03a78bac41e9b0ed6c3ae29dd3b3fb79f8f
Jason Fang <jason.fang@guidedogs.org.uk>
2022-04-06 01:33:51 +0100

    Swirl CSS ver. sub changes. 100% postlist width.

--- components/Header/index.js
+++ components/Header/index.js
@@ -33,102 +33,106 @@ export default function Header() {
 
   return (
     <>
-      <Flex
-        backgroundColor={"brand.100"}
-        maxHeight="180"
-        height="142"
-        alignItems="center"
-        alignContent="start"
-      >
-        <Box maxWidth="1px" maxHeight="1px" margin="0">
-          <div className={HeaderStyles.skipLinks}>
-            <div className={HeaderStyles.skipLinkslink}>
-            <ul>
-              <li>
-                <a href="/covid19">Coronavirus (COVID-19) and Guide Dogs</a>
-              </li>
-              <li>
-                <a href="#content" class="skip-to-content">
-                  Skip to content
-                </a>
-              </li>
-              <li>
-                <a href="#footer" class="skip-to-content">
-                  Skip to footer
-                </a>
-              </li>
-            </ul>
+      <Box zIndex="999" className={HeaderStyles.header}>
+        <Flex
+          backgroundColor={"brand.100"}
+          maxHeight="180"
+          height="142"
+          alignItems="center"
+          alignContent="start"
+          zIndex="999"
+         
+        >
+          <Box maxWidth="1px" maxHeight="1px" margin="0">
+            <div className={HeaderStyles.skipLinks}>
+              <div className={HeaderStyles.skipLinkslink}>
+                <ul>
+                  <li>
+                    <a href="/covid19">Coronavirus (COVID-19) and Guide Dogs</a>
+                  </li>
+                  <li>
+                    <a href="#content" class="skip-to-content">
+                      Skip to content
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#footer" class="skip-to-content">
+                      Skip to footer
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
-          </div>
-        </Box>
-
-        <Box alignSelf="center" p={10}>
-          <Link href="https://www.guidedogs.org.uk">
-            <a
-              className={HeaderStyles.header__logoContainerLink}
-              aria-label="Navigate to home page"
-            >
-              <Logo />
-            </a>
-          </Link>
-        </Box>
-        <Spacer />
-
-        <Flex display="none">
-          <Box>
-            <Link href="https://www.guidedogs.org.uk/ensuring-our-website-is-accessible/">
-              <a className={HeaderStyles.header_headerItem}>
-                <Box className={HeaderStyles.header_headerItem}>
-                  <u>Ensuring our website is accessible</u>
-                </Box>
-              </a>
-            </Link>
           </Box>
-          <Box>
-            <Link
-              href="https://guidedogsshop.com/?_ga=2.12167092.1043349381.1635768433-1104808427.1634557046"
-              passHref
-            >
+
+          <Box alignSelf="center" p={10}>
+            <Link href="https://www.guidedogs.org.uk">
               <a
-                className={HeaderStyles.header_headerItem}
-                target="_blank"
-                rel="noreferrer"
+                className={HeaderStyles.header__logoContainerLink}
+                aria-label="Navigate to home page"
               >
-                <Box className={HeaderStyles.header_headerItem}>
-                  <u>Shop</u>
-                  <Icon as={MdOpenInNew} />
-                </Box>
+                <Logo />
               </a>
             </Link>
           </Box>
-          <Box>
-            <a className={HeaderStyles.header_headerItem}>
-              <Box className={HeaderStyles.header_headerItemm}>
-                <u>Search</u>
-              </Box>
-            </a>
-          </Box>
-          <Box>
-            <Link href="https://www.guidedogs.org.uk/how-you-can-help/donating/sponsor-a-puppy/">
+          <Spacer />
+
+          <Flex display="none">
+            <Box>
+              <Link href="https://www.guidedogs.org.uk/ensuring-our-website-is-accessible/">
+                <a className={HeaderStyles.header_headerItem}>
+                  <Box className={HeaderStyles.header_headerItem}>
+                    <u>Ensuring our website is accessible</u>
+                  </Box>
+                </a>
+              </Link>
+            </Box>
+            <Box>
+              <Link
+                href="https://guidedogsshop.com/?_ga=2.12167092.1043349381.1635768433-1104808427.1634557046"
+                passHref
+              >
+                <a
+                  className={HeaderStyles.header_headerItem}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Box className={HeaderStyles.header_headerItem}>
+                    <u>Shop</u>
+                    <Icon as={MdOpenInNew} />
+                  </Box>
+                </a>
+              </Link>
+            </Box>
+            <Box>
               <a className={HeaderStyles.header_headerItem}>
-                <Box color="brand.100">
-                  <Button
-                    bgColor="#8FD8FF"
-                    fontSize="xl"
-                    fontWeight="bold"
-                    padding="7"
-                    aria-label="Sponsor a puppy"
-                    className={HeaderStyles.header__headerButton}
-                  >
-                    Sponsor a puppy
-                  </Button>
+                <Box className={HeaderStyles.header_headerItemm}>
+                  <u>Search</u>
                 </Box>
               </a>
-            </Link>
-          </Box>
+            </Box>
+            <Box>
+              <Link href="https://www.guidedogs.org.uk/how-you-can-help/donating/sponsor-a-puppy/">
+                <a className={HeaderStyles.header_headerItem}>
+                  <Box color="brand.100">
+                    <Button
+                      bgColor="#8FD8FF"
+                      fontSize="xl"
+                      fontWeight="bold"
+                      padding="7"
+                      aria-label="Sponsor a puppy"
+                      className={HeaderStyles.header__headerButton}
+                    >
+                      Sponsor a puppy
+                    </Button>
+                  </Box>
+                </a>
+              </Link>
+            </Box>
+          </Flex>
+          <Spacer />
         </Flex>
-        <Spacer />
-      </Flex>
+      </Box>
 
       <Flex
         role="navigation"
--- components/PostTopSection/index.js
+++ components/PostTopSection/index.js
@@ -7,6 +7,7 @@ import ReactMarkdown from "react-markdown";
 import PublishedDate from "@components/Post/PublishedDate";
 import ContentWrapper from "@components/ContentWrapper";
 import ContentWrapperStyle from "@styles/ContentWrapper.module.css";
+import { Config } from "@utils/Config";
 
 import {
   Box,
@@ -34,19 +35,150 @@ export default function PostTopSection(props) {
 
   return (
     <>
-      <Box display={{ base: "none", lg: "block" }}>
-        <ContentWrapper>
-          <Box
-            style={{
-              position: "absolute",
-              top: "240px",
-              paddingRight: "128px",
-              maxWidth: "1280px",
-              width: "100%",
-            }}
+      <Box bgColor={{ base: "#FFDF7F", lg: "transparent" }}>
+        <Box display={{ base: "none", lg: "block" }}  height="450">
+          <ContentWrapper>
+            <div
+              className={ContentListStyles.ellipse}
+              style={{ top: "-600px", left: "-600px" }}
+            />
+
+            <Box style={{ position: "absolute", top: "142px" }}>
+              <Flex alignItems="center" pb={0} pt={5}>
+                <Box alignSelf="center">
+                  <Flex
+                    className={ContentListStyles.contentList__homelink}
+                    minWidth="160"
+                  >
+                    <Box style={{ textDecoration: "underline" }}>
+                      <Link href="https://www.guidedogs.org.uk">
+                        <a>
+                          <h3>Home</h3>
+                        </a>
+                      </Link>
+                    </Box>
+                    <Spacer />
+                    <Box>></Box>
+                    <Spacer />
+                    <Box style={{ textDecoration: "underline" }}>
+                      <Link href={`${Config.pageMeta.home.slug}`}>
+                        <a>
+                          <h3>Blog</h3>
+                        </a>
+                      </Link>
+                    </Box>
+                  </Flex>
+                </Box>
+                <Spacer />
+              </Flex>
+            </Box>
+          </ContentWrapper>
+        </Box>
+
+        <Box
+          alignItems="center"
+          pt={2}
+          pl={3}
+          display={{ base: "block", lg: "none" }}
+          bgColor="white"
+          pb={0.5}
+         
+        >
+          <Flex
+            className={ContentListStyles.contentList__homelink}
+            maxW="52"
+            alignSelf="center"
           >
-            <Flex>
-              <Box maxWidth={600} pr={5}>
+            <Box style={{ textDecoration: "underline" }}>
+              <Link href="https://www.guidedogs.org.uk">
+                <a>
+                  <h3>Home</h3>
+                </a>
+              </Link>
+            </Box>
+            <Spacer />
+            <Box>></Box>
+            <Spacer />
+            <Box style={{ textDecoration: "underline" }}>
+              <Link href={`${Config.pageMeta.home.slug}`}>
+                <a>
+                  <h3>Blog</h3>
+                </a>
+              </Link>
+            </Box>
+            <Spacer />
+          </Flex>
+        </Box>
+
+        <Box pt={10} pb={5}>
+          <Box display={{ base: "none", lg: "block" }}>
+            <ContentWrapper>
+              <Box
+                style={{
+                  position: "absolute",
+                  top: "240px",
+                  paddingRight: "4rem",
+                  maxWidth: "75rem",
+                  width: "100%",
+                }}
+              >
+                <Flex>
+                  <Box maxWidth={600} pr={5}>
+                    <h1 className={TypographyStyles.heading__h1post}>
+                      {featuredPost.title}
+                    </h1>
+                    <Box alignSelf="center" className={TypographyStyles.date}>
+                      <b>Date: </b>
+                      <PublishedDate
+                        date={featuredPost.date}
+                        alignSelf="center"
+                      />
+                    </Box>
+                    <Box className={TypographyStyles.author}>
+                      {featuredPost.author !== null && (
+                        <>
+                          {" "}
+                          <b>Author: {featuredPost.author.name}</b>
+                        </>
+                      )}
+                    </Box>
+
+                    {featuredPost.contentfulMetadata.tags !== null && (
+                      <Tags tags={featuredPost.contentfulMetadata.tags} />
+                    )}
+                  </Box>
+                  <Spacer />
+                  <Box maxWidth={600}>
+                    <img
+                      src={featuredPost.image.url}
+                      width="1200"
+                      layout="responsive"
+                      objectFit="contain"
+                      alt={featuredPost.image.description}
+                      style={{ borderRadius: "20px" }}
+                    />
+                  </Box>
+                </Flex>
+              </Box>
+            </ContentWrapper>
+          </Box>
+
+          <Flex display={{ base: "block", lg: "none" }}>
+            <ContentWrapper>
+              <article
+                className={ContentListStyles.contentList__post}
+                style={{ marginBottom: "0px" }}
+              >
+                <img
+                  src={featuredPost.image.url}
+                  layout="responsive"
+                  alt={featuredPost.image.description}
+                  style={{
+                    borderRadius: "20px",
+                    objectFit: "cover",
+                  }}
+                />
+                <Flex pt={5} />
                 <h1 className={TypographyStyles.heading__h1post}>
                   {featuredPost.title}
                 </h1>
@@ -54,74 +186,24 @@ export default function PostTopSection(props) {
                   <b>Date: </b>
                   <PublishedDate date={featuredPost.date} alignSelf="center" />
                 </Box>
-                <Box className={TypographyStyles.author}>
+                <Box className={ContentListStyles.contentList__author}>
                   {featuredPost.author !== null && (
                     <>
                       {" "}
-                      <b>Author: {featuredPost.author.name}</b>
+                      <b> Author: {featuredPost.author.name} </b>
                     </>
                   )}
                 </Box>
-
-                {featuredPost.contentfulMetadata.tags !== null && (
-                  <Tags tags={featuredPost.contentfulMetadata.tags} />
-                )}
-              </Box>
-              <Spacer />
-              <Box maxWidth={600}>
-                <img
-                  src={featuredPost.image.url}
-                  width="1200"
-                  layout="responsive"
-                  objectFit="contain"
-                  alt={featuredPost.image.description}
-                  style={{ borderRadius: "20px" }}
-                />
-              </Box>
-            </Flex>
-          </Box>
-        </ContentWrapper>
+                <Box pb={5}>
+                  {featuredPost.contentfulMetadata.tags !== null && (
+                    <Tags tags={featuredPost.contentfulMetadata.tags} />
+                  )}
+                </Box>
+              </article>
+            </ContentWrapper>
+          </Flex>
+        </Box>
       </Box>
-
-      <Flex display={{ base: "block", lg: "none" }}>
-        <ContentWrapper>
-          <article
-            className={ContentListStyles.contentList__post}
-            style={{ marginBottom: "0px" }}
-          >
-            <img
-              src={featuredPost.image.url}
-              layout="responsive"
-              alt={featuredPost.image.description}
-              style={{
-                borderRadius: "20px",
-                objectFit: "cover",
-              }}
-            />
-            <Flex pt={5} />
-            <h1 className={TypographyStyles.heading__h1post}>
-              {featuredPost.title}
-            </h1>
-            <Box alignSelf="center" className={TypographyStyles.date}>
-              <b>Date: </b>
-              <PublishedDate date={featuredPost.date} alignSelf="center" />
-            </Box>
-            <Box className={ContentListStyles.contentList__author}>
-              {featuredPost.author !== null && (
-                <>
-                  {" "}
-                  <b> Author: {featuredPost.author.name} </b>
-                </>
-              )}
-            </Box>
-            <Box pb={5}>
-              {featuredPost.contentfulMetadata.tags !== null && (
-                <Tags tags={featuredPost.contentfulMetadata.tags} />
-              )}
-            </Box>
-          </article>
-        </ContentWrapper>
-      </Flex>
     </>
   );
 }
