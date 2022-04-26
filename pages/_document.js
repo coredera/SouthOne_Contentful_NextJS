97edf9d2303bb126bcdc0db491fa1531939f7953
Jason Fang <jason.fang@guidedogs.org.uk>
2022-04-26 11:43:38 +0100

    Cookie banner update script

--- pages/_document.js
+++ pages/_document.js
@@ -39,7 +39,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             dangerouslySetInnerHTML={{
               __html: `
               <!-- OneTrust Cookies Consent Notice start for gd-blog.netlify.app/blog -->
-
 <script src="https://cdn-ukwest.onetrust.com/consent/eba07de4-b14e-4196-abee-65680168966e/otSDKStub.js"  type="text/javascript" charset="UTF-8" data-domain-script="eba07de4-b14e-4196-abee-65680168966e" ></script>
 <script type="text/javascript">
 function OptanonWrapper() { }
