c9b811b3f26b152d004c30ee18335c5ccbea8266
Jason Fang <jason.fang@guidedogs.org.uk>
2022-05-04 17:05:22 +0100

    removed trailing slash

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   basePath: '/blog',
   //assetPrefix: '/blog',
   //assetPrefix: './',
-  trailingSlash: true,
+  //trailingSlash: true,
   //exportTrailingSlash: true,
   images: {
     domains: ["images.ctfassets.net", "gd-blog.netlify.app", "www.guidedogs.org.uk", "gd-blog-dev.netlify.app", "gd-blog-staging.netlify.app"],
