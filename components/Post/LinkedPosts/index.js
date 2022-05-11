49808910169d756a4a51967f71ab90f90bd92dda
Jason Fang <jason.fang@guidedogs.org.uk>
2022-05-11 10:37:14 +0100

    linkedposts removed log, same height pods

--- components/Post/LinkedPosts/index.js
+++ components/Post/LinkedPosts/index.js
@@ -33,10 +33,10 @@ import {
 
 export default function LinkedPosts(props) {
   const { posts } = props;
-  console.log(posts);
+  //console.log(posts);
+  //display="flex"
 
   return (
-
     <>
       <ContentWrapper>
         <Box>
@@ -44,73 +44,82 @@ export default function LinkedPosts(props) {
             Related posts...
           </Flex>
           <Flex direction={{ base: "column", lg: "row" }}>
-            {posts.map((post, i) => (
-                post ?
-              <>
-                <Box key={post.sys.id} width="full" height="full" pb={10} flexDirection="column" flexFlow="column" flexGrow="1" display="flex" flex="1 1 auto">
-                  <Box className={ContentListStyles.pods}>
-                    <article>
-                      <Box>
+            {posts.map((post, i) =>
+              post ? (
+                <>
+                  <Box
+                    key={post.sys.id}
+                    width="full"
+                    height="full"
+                    pb={10}
+                    flexDirection="column"
+                    flexFlow="column"
+                    flexGrow="1"
+                    display="flex"
+                    flex="1 1 auto"
+                  >
+                    <Box className={ContentListStyles.pods}>
+                      <article>
                         <Box>
-                          <Link href={`/${post.slug}`}>
-                            <a>
-                              <Box>
-                                <img
-                                  src={`${post.image.url}?fm=jpg&q=50`}
-                                  alt={post.image.description}
-                                  layout="responsive"
-                                  style={{
-                                    borderTopLeftRadius: "10px",
-                                    borderTopRightRadius: "10px",
-                                    objectFit: "cover",
-                                    width: "80rem",
-                                   
-                                  }}
-                                />
-                              </Box>
-                            </a>
-                          </Link>
-                        </Box>
-                      </Box>
-
-                      <Box p={8}>
-                        <Flex pb={0}>
                           <Box>
                             <Link href={`/${post.slug}`}>
                               <a>
-                                <h2
-                                  className={`${TypographyStyles.heading__h6} ${TypographyStyles.inlineLink}`}
-                                  style={{ marginBottom: "0" }}
-                                >
-                                  {post.title}
-                                </h2>
+                                <Box>
+                                  <img
+                                    src={`${post.image.url}?fm=jpg&q=50`}
+                                    alt={post.image.description}
+                                    layout="responsive"
+                                    style={{
+                                      borderTopLeftRadius: "10px",
+                                      borderTopRightRadius: "10px",
+                                      objectFit: "cover",
+                                      width: "80rem",
+                                    }}
+                                  />
+                                </Box>
                               </a>
                             </Link>
                           </Box>
-                        </Flex>
-
-                        <Box pb={8} pt={6}>
-                          <div
-                            className={ContentListStyles.contentList__excerpt}
-                            style={{ marginBottom: "-3rem" }}
-                          >
-                            <ReactMarkdown
-                              children={post.excerpt}
-                              renderers={ReactMarkdownRenderers(post.excerpt)}
-                            />
-                          </div>
                         </Box>
-                      
-                      </Box>
-                    </article>
-                  </Box>
-                </Box>
 
-                {i < 2 ? <Spacer p={5} /> : ""}
-                                
-              </> :""
-                                
-            ))}
+                        <Box p={8}>
+                          <Flex pb={0}>
+                            <Box>
+                              <Link href={`/${post.slug}`}>
+                                <a>
+                                  <h2
+                                    className={`${TypographyStyles.heading__h6} ${TypographyStyles.inlineLink}`}
+                                    style={{ marginBottom: "0" }}
+                                  >
+                                    {post.title}
+                                  </h2>
+                                </a>
+                              </Link>
+                            </Box>
+                          </Flex>
+
+                          <Box pb={8} pt={6}>
+                            <div
+                              className={ContentListStyles.contentList__excerpt}
+                              style={{ marginBottom: "-3rem" }}
+                            >
+                              <ReactMarkdown
+                                children={post.excerpt}
+                                renderers={ReactMarkdownRenderers(post.excerpt)}
+                              />
+                            </div>
+                          </Box>
+                        </Box>
+                      </article>
+                    </Box>
+                  </Box>
+
+                  {i < 2 ? <Spacer p={5} /> : ""}
+                </>
+              ) : (
+                ""
+              ),
+            )}
           </Flex>
         </Box>
       </ContentWrapper>
